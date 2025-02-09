4d8a2f0707e968decddd43312bfea3d78a84201a
Dogubo Mamuzo Joshua <emason.tech@gmail.com>
2025-02-09 04:13:45 +0100

    plash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,40 @@
+// // import React from "react";
+// // import { Stack } from "expo-router";
+// // import { Provider } from "react-redux";
+// // import { PersistGate } from "redux-persist/integration/react";
+// // import { ActivityIndicator, View } from "react-native";
+// // import { persistor, store } from "./redux/store";
+
+// // const RootLayout = () => {
+// //   return (
+// //     <Provider store={store}>
+// //       <PersistGate
+// //         loading={
+// //           <View
+// //             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+// //           >
+// //             <ActivityIndicator size="large" color="#0000ff" />
+// //           </View>
+// //         }
+// //         persistor={persistor}
+// //       >
+// //         <Stack>
+// //           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+// //           <Stack.Screen name="index" options={{ headerShown: false }} />
+// //         </Stack>
+// //       </PersistGate>
+// //     </Provider>
+// //   );
+// // };
+
+// // export default RootLayout;
+
 // import React from "react";
 // import { Stack } from "expo-router";
 // import { Provider } from "react-redux";
 // import { PersistGate } from "redux-persist/integration/react";
 // import { ActivityIndicator, View } from "react-native";
-// import { persistor, store } from "./redux/store";
+// import store, { persistor } from "./redux/store";
 
 // const RootLayout = () => {
 //   return (
@@ -18,10 +49,9 @@
 //         }
 //         persistor={persistor}
 //       >
-//         <Stack>
-//           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-//           <Stack.Screen name="index" options={{ headerShown: false }} />
-//         </Stack>
+//         <Stack
+//           screenOptions={{ title: "Welcome Switcher", headerShown: false }}
+//         />
 //       </PersistGate>
 //     </Provider>
 //   );
@@ -29,32 +59,33 @@
 
 // export default RootLayout;
 
-import React from "react";
 import { Stack } from "expo-router";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { ActivityIndicator, View } from "react-native";
-import store, { persistor } from "./redux/store";
+import { useEffect, useState } from "react";
+import { View, ActivityIndicator } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import React from "react";
 
-const RootLayout = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate
-        loading={
-          <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          >
-            <ActivityIndicator size="large" color="#0000ff" />
-          </View>
-        }
-        persistor={persistor}
-      >
-        <Stack
-          screenOptions={{ title: "Welcome Switcher", headerShown: false }}
-        />
-      </PersistGate>
-    </Provider>
-  );
-};
+export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
 
-export default RootLayout;
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+      setTimeout(() => {
+        setIsReady(true);
+        SplashScreen.hideAsync();
+      }, 2000);
+    }
+    prepare();
+  }, []);
+
+  if (!isReady) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return <Stack />;
+}
